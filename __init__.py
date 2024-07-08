0f2fa5a939904d1931524000daee601724233050
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2024-07-08 03:26:43 +0200

    Added module docstring.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+"""mathio, A better Python package for math.
+
+mathio is a Python package designed to make mathematical operations easier, faster, and more powerful. Its goal is to provide a clean and intuitive package for a wide range of mathematical functions.
+"""
+
 from .errors import *
 from .constants import *
 from .functions import *
